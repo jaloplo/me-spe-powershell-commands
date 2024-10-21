9b748adde80d6bb31aea72b151067c0cd96676af
Jaime López <jaime.lopez.lopez@live.com>
2024-10-21 09:37:20 +0200

    feat: add all containers to be removed providing the container type id

--- src/Remove-SPEContainer.ps1
+++ src/Remove-SPEContainer.ps1
@@ -1,18 +1,25 @@
 <#
 .SYNOPSIS
-    Deletes a container.
+    Deletes a container or all containers for the specified ContainerTypeId.
 .DESCRIPTION
-    Deletes a container for the specified ContainerId.
+    Deletes a container for the specified ContainerId or all containers for the specified ContainerTypeId.
 .PARAMETER ContainerId
     The ContainerId of the container to delete.
+.PARAMETER ContainerTypeId
+    The ContainerTypeId of the containers to delete.
 .EXAMPLE
     PS C:\> Remove-SPEContainer -ContainerId <Your Container Id>
     Deletes the container with the specified ContainerId.
+.EXAMPLE
+    PS C:\> Remove-SPEContainer -ContainerTypeId <Your Container Type Id>
+    Deletes all containers for the specified ContainerTypeId.
 #>
 
 param(
-    [Parameter(Mandatory=$true)]
-    [string] $ContainerId
+    [Parameter(ParameterSetName='One', Mandatory=$true)]
+    [string] $ContainerId,
+    [Parameter(ParameterSetName='All', Mandatory=$true)]
+    [string] $ContainerTypeId
 )
 
 $isConnected = $null -ne $(Get-MgContext)
@@ -22,9 +29,22 @@ If(-not $isConnected) {
 }
 
 Try {
-    $Request = Invoke-MgGraphRequest -Method DELETE -Uri "https://graph.microsoft.com/v1.0/storage/fileStorage/containers/$ContainerId" -Body $Body -ErrorAction Stop
+    Switch($PSCmdlet.ParameterSetName) {
+        'All' {
+            $ContainersData = .\Get-SPEContainer -ContainerTypeId $ContainerTypeId
+            $ContainersCollection = $(ConvertFrom-Json $ContainersData).value
 
-    $Request
+            foreach($Container in $ContainersCollection) {
+                $ContainerId = $Container.id
+                $Request = Invoke-MgGraphRequest -Method DELETE -Uri "https://graph.microsoft.com/v1.0/storage/fileStorage/containers/$ContainerId" -Body $Body -ErrorAction Stop
+            }
+        }
+        'One' {
+            $Request = Invoke-MgGraphRequest -Method DELETE -Uri "https://graph.microsoft.com/v1.0/storage/fileStorage/containers/$ContainerId" -Body $Body -ErrorAction Stop
+    
+            $Request
+        }
+    }
 }
 Catch {
     Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
